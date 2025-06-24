0152ba904811bf3a15f960bcf28d05142515bc6a
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2025-06-24 10:19:23 +0530

    fix

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Toaster } from "sonner";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,7 +36,14 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by Anamika Jha</p>
+                <p>
+                  Made with 💗 by{" "}
+                  <Link href="https://www.linkedin.com/in/anamika-jha-6608382a6/">
+                    <span className="font-semibold text-sky-100 hover:text-white transition-all duration-300 tracking-wide drop-shadow-[0_0_2px_rgba(135,206,250,0.8)]">
+                      Anamika Jha
+                    </span>
+                  </Link>{" "}
+                </p>
               </div>
             </footer>
           </ThemeProvider>
